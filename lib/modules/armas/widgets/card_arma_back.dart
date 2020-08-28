36191f57e67b0244264c4893d65e7a6c36408c65
kauemurakami <kauetmurakami@gmail.com>
2020-08-28 17:43:28 -0300

    fix: altura stringer

--- lib/modules/armas/widgets/card_arma_back.dart
+++ lib/modules/armas/widgets/card_arma_back.dart
@@ -10,171 +10,177 @@ class CardArmaBack extends StatelessWidget {
   CardArmaBack(this.arma);
   @override
   Widget build(BuildContext context) {
-    return Stack(children: [
-      Container(
-        child: Transform(
-            alignment: Alignment.center,
-            transform: Matrix4.rotationY(math.pi),
-            child: Container(
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: NetworkImage(
-                          'https://raw.githubusercontent.com/kauemurakami/valorant-br-api/master/images/armas/${arma.nome.toLowerCase()}.png'))),
-            )),
-      ),
-      Container(
-        margin: EdgeInsets.only(left: 24.0),
-        height: 250,
-        width: Get.width,
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Container(
-              padding: EdgeInsets.only(top: 8.0, right: 8.0),
-              width: Get.width,
-              child: Text(
-                arma.descricao,
-                style: bannerArmaBack,
-              ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 8.0),
-              height: 30,
-              width: Get.width,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                      width: 80,
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 1, color: mainColor)),
-                      child: Center(
-                          child: Text(
-                        'Distância',
-                        style: TextStyle(color: mainColor),
-                      ))),
-                  Expanded(
-                    child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: arma.distncia.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                              height: 70,
-                              width: 80,
-                              decoration: BoxDecoration(
-                                  border:
-                                      Border.all(width: 1, color: mainColor)),
-                              child: Center(
-                                child: Text(
-                                  arma.distncia[index],
-                                  style: TextStyle(color: mainColor),
-                                ),
-                              ));
-                        }),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-                height: 30,
-                width: Get.width,
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                  Container(
-                      width: 80,
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 1, color: mainColor)),
-                      child: Center(
-                          child: Text(
-                        'Cabeça',
-                        style: TextStyle(color: mainColor),
-                      ))),
-                  Expanded(
-                    child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: arma.dano.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                              height: 70,
-                              width: 80,
-                              decoration: BoxDecoration(
-                                  border:
-                                      Border.all(width: 1, color: mainColor)),
-                              child: Center(
-                                  child: Text(arma.dano[index].cabeca,
-                                      style: TextStyle(color: mainColor))));
-                        }),
-                  ),
-                ])),
-            Container(
-                height: 30,
-                width: Get.width,
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                  Container(
-                      width: 80,
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 1, color: mainColor)),
-                      child: Center(
-                          child: Text(
-                        'Corpo',
-                        style: TextStyle(color: mainColor),
-                      ))),
-                  Expanded(
-                    child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: arma.dano.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                              height: 70,
-                              width: 80,
-                              decoration: BoxDecoration(
-                                  border:
-                                      Border.all(width: 1, color: mainColor)),
-                              child: Center(
-                                  child: Text(
-                                arma.dano[index].corpo,
-                                style: TextStyle(color: mainColor),
-                              )));
-                        }),
-                  ),
-                ])),
-            Container(
-                height: 30,
-                width: Get.width,
-                child:
-                    Row(mainAxisAlignment: MainAxisAlignment.start, children: [
-                  Container(
-                      width: 80,
-                      decoration: BoxDecoration(
-                          border: Border.all(width: 1, color: mainColor)),
-                      child: Center(
-                          child: Text(
-                        'Pernas',
-                        style: TextStyle(color: mainColor),
-                      ))),
-                  Expanded(
-                    child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: arma.dano.length,
-                        itemBuilder: (context, index) {
-                          return Container(
-                              height: 70,
-                              width: 80,
-                              decoration: BoxDecoration(
-                                  border:
-                                      Border.all(width: 1, color: mainColor)),
-                              child: Center(
-                                  child: Text(
-                                arma.dano[index].pernas,
-                                style: TextStyle(color: mainColor),
-                              )));
-                        }),
-                  )
-                ])),
-          ],
+    return Container(
+      height: 250,
+      child: Stack(children: [
+        Container(
+          child: Transform(
+              alignment: Alignment.center,
+              transform: Matrix4.rotationY(math.pi),
+              child: Container(
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(
+                            'https://raw.githubusercontent.com/kauemurakami/valorant-br-api/master/images/armas/${arma.nome.toLowerCase()}.png'))),
+              )),
         ),
-      ),
-    ]);
+        Container(
+          margin: EdgeInsets.only(left: 24.0),
+          width: Get.width,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Container(
+                padding: EdgeInsets.only(top: 8.0, right: 8.0),
+                width: Get.width,
+                child: Text(
+                  arma.descricao,
+                  style: bannerArmaBack,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 8.0),
+                height: 30,
+                width: Get.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                        width: 80,
+                        decoration: BoxDecoration(
+                            border: Border.all(width: 1, color: mainColor)),
+                        child: Center(
+                            child: Text(
+                          'Distância',
+                          style: TextStyle(color: mainColor),
+                        ))),
+                    Expanded(
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: arma.distncia.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                                height: 70,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                    border:
+                                        Border.all(width: 1, color: mainColor)),
+                                child: Center(
+                                  child: Text(
+                                    arma.distncia[index],
+                                    style: TextStyle(color: mainColor),
+                                  ),
+                                ));
+                          }),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                  height: 30,
+                  width: Get.width,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                            width: 80,
+                            decoration: BoxDecoration(
+                                border: Border.all(width: 1, color: mainColor)),
+                            child: Center(
+                                child: Text(
+                              'Cabeça',
+                              style: TextStyle(color: mainColor),
+                            ))),
+                        Expanded(
+                          child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: arma.dano.length,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                    height: 70,
+                                    width: 80,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            width: 1, color: mainColor)),
+                                    child: Center(
+                                        child: Text(arma.dano[index].cabeca,
+                                            style:
+                                                TextStyle(color: mainColor))));
+                              }),
+                        ),
+                      ])),
+              Container(
+                  height: 30,
+                  width: Get.width,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                            width: 80,
+                            decoration: BoxDecoration(
+                                border: Border.all(width: 1, color: mainColor)),
+                            child: Center(
+                                child: Text(
+                              'Corpo',
+                              style: TextStyle(color: mainColor),
+                            ))),
+                        Expanded(
+                          child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: arma.dano.length,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                    height: 70,
+                                    width: 80,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            width: 1, color: mainColor)),
+                                    child: Center(
+                                        child: Text(
+                                      arma.dano[index].corpo,
+                                      style: TextStyle(color: mainColor),
+                                    )));
+                              }),
+                        ),
+                      ])),
+              Container(
+                  height: 30,
+                  width: Get.width,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                            width: 80,
+                            decoration: BoxDecoration(
+                                border: Border.all(width: 1, color: mainColor)),
+                            child: Center(
+                                child: Text(
+                              'Pernas',
+                              style: TextStyle(color: mainColor),
+                            ))),
+                        Expanded(
+                          child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: arma.dano.length,
+                              itemBuilder: (context, index) {
+                                return Container(
+                                    height: 70,
+                                    width: 80,
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                            width: 1, color: mainColor)),
+                                    child: Center(
+                                        child: Text(
+                                      arma.dano[index].pernas,
+                                      style: TextStyle(color: mainColor),
+                                    )));
+                              }),
+                        )
+                      ])),
+            ],
+          ),
+        ),
+      ]),
+    );
   }
 }
